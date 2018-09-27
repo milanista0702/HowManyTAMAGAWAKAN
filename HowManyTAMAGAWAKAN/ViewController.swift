1680e85344b632cc8be743811d4ceeaad9e61fe2
Makka Misako <real0205-cris@ezweb.ne.jp>
2018-09-27 21:04:30 +0900

    Initial Commit

---
+++ HowManyTAMAGAWAKAN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowManyTAMAGAWAKAN
+//
+//  Created by misako makka on 2018/09/27.
+//  Copyright © 2018年 misako makka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
